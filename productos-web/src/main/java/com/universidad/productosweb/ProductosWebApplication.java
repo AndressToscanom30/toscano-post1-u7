8deda60ceaf0b2606128722de4b4d46f9f356104
Andres Toscano <andress.toscano@gmail.com>
2026-04-15 13:04:34 -0500

    Estructura inicial del proyecto de springboot

---
+++ productos-web/src/main/java/com/universidad/productosweb/ProductosWebApplication.java
@@ -0,0 +1,15 @@
+package com.universidad.productoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosWebApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProductosWebApplication.class, args);
+
+    }
+
+}
